e09c0bcf0d279269d37e86a35563bfb14684f028
Nandaja <nandaja.varma@gmail.com>
2021-04-09 12:45:54 +0200

    feat: adding the front end stub and data struct for the AJA

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/nandajavarma
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/rs/cors v1.7.0
+	github.com/sirupsen/logrus v1.8.1
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
+)
--- main.go
+++ main.go
@@ -1,25 +1,149 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/rs/cors"
+	log "github.com/sirupsen/logrus"
+
+	// _ infront of import means it is merely injecting side-effects
+	// In layman's terms basically mean create package-level variable and execute
+	// init function of the package
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+var db, _ = gorm.Open("mysql", "root:root@/ajadb?charset=utf8&parseTime=True&loc=Local")
+
+type TodoItemModel struct {
+	Id          int `gorm:"primary_key"`
+	Description string
+	Completed   bool
+}
+
+func getItemByID(Id int) bool {
+	todo := &TodoItemModel{}
+	result := db.First(&todo, Id)
+	if result.Error != nil {
+		log.Warn("Activity not found in the database")
+		return false
+	}
+	return true
+}
+
+func updateItem(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	err := getItemByID(id)
+	if err == false {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": false, "error": "Record not found"}`)
+	} else {
+		completed, _ := strconv.ParseBool(r.FormValue("completed"))
+		log.WithFields(log.Fields{"id": id, "Completed": completed}).Info("Updating activity")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		todo.Completed = completed
+		db.Save(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": true}`)
+	}
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	err := getItemByID(id)
+	if err == false {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"deleted": false, "error": "record not found"}`)
+	} else {
+		log.WithFields(log.Fields{"id": id}).Info("Deleting the item")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		db.Delete(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"deleted": true}`)
+	}
+}
+
+func getTodoItems(completed bool) interface{} {
+	var todos []TodoItemModel
+	TodoItems := db.Where("completed = ?", completed).Find(&todos).Value
+	return TodoItems
+}
+
+func getCompletedItems(w http.ResponseWriter, r *http.Request) {
+	log.Info("Get completed activities")
+	completedTodoItems := getTodoItems(true)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(completedTodoItems)
+}
+
+func getIncompleteItems(w http.ResponseWriter, r *http.Request) {
+	log.Info("Get incomplete activities")
+	incompleteTodoItems := getTodoItems(false)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(incompleteTodoItems)
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+	description := r.FormValue("description")
+	log.WithFields(log.Fields{"description": description}).Info("Add new activity, saving to db")
+	todo := &TodoItemModel{Description: description, Completed: false}
+	db.Create(&todo)
+	result := db.Last(&todo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result.Value)
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	log.Info("API Health is OK")
+	w.Header().Set("Content-Type", "application/json")
+	// `` declares raw strings inside which it is legal to have any characters
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
 
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
+	r.HandleFunc("/healthz", healthz).Methods("GET")
 	r.HandleFunc("/app", handler).Methods("GET")
+	r.HandleFunc("/todo", createItem).Methods("POST")
+	r.HandleFunc("/todo-complete", getCompletedItems).Methods("Get")
+	r.HandleFunc("/todo-incomplete", getIncompleteItems).Methods("Get")
+	r.HandleFunc("/todo/{id}", updateItem).Methods("POST")
+	r.HandleFunc("/todo/{id}", deleteItem).Methods("DELETE")
 	return r
 
 }
 
-func main() {
-
-	r := newRouter()
-	http.ListenAndServe(":8080", r)
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetReportCaller(true)
 }
 
+func main() {
+	defer db.Close()
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	db.Debug().DropTableIfExists(&TodoItemModel{}) // I doubt if this is a good practice
+	db.Debug().AutoMigrate(&TodoItemModel{})
+
+	log.Info("Starting AJA API server")
+	r := newRouter()
+	handler := cors.New(cors.Options{
+		AllowedMethods: []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS"},
+	}).Handler(r)
+	http.ListenAndServe(":8080", handler)
 }
